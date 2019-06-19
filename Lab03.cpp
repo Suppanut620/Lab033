e1c5d563a4c40bbbc55356e44a2c9104c85f331f
Suppanut620 <6206021610056@fitm.kmutnb.ac.th>
2019-06-19 23:04:12 +0700

    Create Lab03.cpp

---
+++ Lab03.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Student Code : 62-060216-1005-6 \n";
+	cout<<"Student Name : Mr.Suppanut Junwilai \n";
+	cout<<"    Division : Information Technology\n";
+	cout<<"        Year : 1\n";
+	cout<<"        Room : RA\n";
+	cout<<"         Sex : Male\n";
+	cout<<"         Age : 18\n";
+	cout<<"               #   \n";
+	cout<<"              # # \n";
+	cout<<"             #   # \n";
+	cout<<"            #     # \n";
+	cout<<"            #######\n";
+	cout<<"            #     #\n";
+	return(0);
+}
